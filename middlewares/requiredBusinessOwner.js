5d1ad81dc42561d52121685e2351dbfe1e59d305
harshthakur066 <harshthakur066@gmail.com>
2020-06-05 19:16:04 +0530

    added business owner middleware

--- middlewares/requiredBusinessOwner.js
+++ middlewares/requiredBusinessOwner.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-const SuperAdminSchema = mongoose.model("SuperAdmin");
+const BusinessOwnerSchema = mongoose.model("BusinessOwner");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,9 +15,9 @@ module.exports = (req, res, next) => {
       return res.status(401).send({ error: "You must be logged in." });
     }
 
-    const { adminId } = payload;
-    const admin = await SuperAdminSchema.findById(adminId);
-    req.admin = admin;
+    const { ownerId } = payload;
+    const owner = await BusinessOwnerSchema.findById(ownerId);
+    req.owner = owner;
     next();
   });
 };
