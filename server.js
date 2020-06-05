23817b1f4b1ba3137cc8a08c191907bb0394a8fc
harshthakur066 <harshthakur066@gmail.com>
2020-06-05 16:08:24 +0530

    added server setup

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// require("./models/");
+
+mongoose.connect(
+  "mongodb+srv://admin:delicon@reservation-system-cluster-wccqj.mongodb.net/apiv1?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connenting to mongo", err);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// require("./routes/")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(PORT);
+});
