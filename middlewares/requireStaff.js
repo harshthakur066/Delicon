384b98d70777b96e513ed1ef1dceacb49f517b66
harshthakur066 <harshthakur066@gmail.com>
2020-06-05 19:37:23 +0530

    added staff middle ware

---
+++ middlewares/requireStaff.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const StaffSchema = mongoose.model("Staff");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).send({ error: "You must be logged in." });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, "MY SECRETE KEY", async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ error: "You must be logged in." });
+    }
+
+    const { staffId } = payload;
+    const staff = await StaffSchema.findById(staffId);
+    req.staff = staff;
+    next();
+  });
+};
