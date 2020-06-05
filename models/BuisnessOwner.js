b2d5e6ed2e2c79326b08d8c340fa7da344ab07bd
harshthakur066 <harshthakur066@gmail.com>
2020-06-05 18:41:10 +0530

    added buisness owner model

---
+++ models/BuisnessOwner.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const { Schema } = mongoose;
+
+const BuisnessOwnerSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+BuisnessOwnerSchema.pre("save", function (next) {
+  const owner = this;
+  if (!owner.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(owner.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      owner.password = hash;
+      next();
+    });
+  });
+});
+
+BuisnessOwnerSchema.methods.comparePassword = function (candidatePassword) {
+  const owner = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, owner.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      if (!isMatch) {
+        return reject(false);
+      }
+      resolve(true);
+    });
+  });
+};
+
+mongoose.model("BuisnessOwner", BuisnessOwnerSchema);
