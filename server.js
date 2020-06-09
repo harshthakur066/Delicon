6a4c880ceec8b4627bf392f7ba9a079c6eec2aa6
prnv241 <pngawade@mitaoe.ac.in>
2020-06-09 18:11:26 +0530

    CORS headers added

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const indexRoutes = require("./routes/indexRoutes");
 
 const app = express();
 
+//CORS
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(bodyParser.json());
 
 // Auth Routes
