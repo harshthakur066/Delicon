7771da48306667c474bdd46edd6274fb866edfe9
harshthakur066 <harshthakur066@gmail.com>
2020-06-10 21:03:10 +0530

    added client setup

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(PORT);
