a214876b88581c30869d531863347b3e2a350b0d
prnv241 <pngawade@mitaoe.ac.in>
2020-06-17 21:25:35 +0530

    Crud of businesses for admin

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Business = require("../models/Business");
 const BusinessOwner = require("../models/BusinessOwner");
 
+const isSuperAdmin = require("../middlewares/requireSuperAdmin");
 const isBusinessOwner = require("../middlewares/requiredBusinessOwner");
 
 // Get all businesses of perticular owner
@@ -75,6 +76,118 @@ router.delete(
       }
     } catch (err) {
       return res.status(500).json({ error: err.message });
+    }
+  }
+);
+
+//Create for admin
+router.post(
+  "/api/v1/admin/businesses/:ownerId",
+  isSuperAdmin,
+  async (req, res) => {
+    const { name, owner, address, details } = req.body;
+    const ownerId = req.params.ownerId;
+    const date = new Date().toLocaleDateString().split("/").reverse();
+    try {
+      const business = new Business({
+        name: name,
+        owner: owner,
+        address: address,
+        ownerId: ownerId,
+        details: details,
+        createdAt: new Date(date[0], date[2], date[1]),
+      });
+      await business.save();
+      try {
+        const currentowner = await BusinessOwner.findById(ownerId);
+        currentowner.businesses.push(business._id);
+        await BusinessOwner.findByIdAndUpdate(ownerId, currentowner);
+        res.json(business);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          error: err.message,
+          message: "Error while adding id to Business Owner!",
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  }
+);
+
+//Read for admin
+router.get(
+  "/api/v1/admin/businesses/:businessId",
+  isSuperAdmin,
+  async (req, res) => {
+    try {
+      const { businessId } = req.params;
+      const business = await Business.findById(businessId);
+      res.json(business);
+    } catch (err) {
+      console.log(err);
+      res.json({ error: err.message });
+    }
+  }
+);
+
+//Update for admin
+router.put(
+  "/api/v1/admin/businesses/:businessId",
+  isSuperAdmin,
+  async (req, res) => {
+    const { businessId } = req.params;
+    let update = { name: req.body.name, details: req.body.details };
+    try {
+      const business = await Business.findByIdAndUpdate(businessId, update);
+      res.json(business);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  }
+);
+
+//Delete for admin
+router.delete(
+  "/api/v1/admin/businesses/:businessId",
+  isSuperAdmin,
+  async (req, res) => {
+    const businessId = req.params.businessId;
+    try {
+      const business = await Business.findByIdAndDelete(businessId);
+      try {
+        const currentowner = await BusinessOwner.findById(business.ownerId);
+        currentowner.businesses = currentowner.businesses.filter(
+          (id) => id != businessId
+        );
+        await BusinessOwner.findByIdAndUpdate(business.ownerId, currentowner);
+        res.json(business);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          error: err.message,
+          message: "Error while adding id to Business Owner!",
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  }
+);
+
+//Businesses of owner for admin
+router.get(
+  "/api/v1/admin/:ownerId/businesses",
+  isSuperAdmin,
+  async (req, res) => {
+    const ownerId = req.params.ownerId;
+    try {
+      const business = await Business.find({ ownerId: ownerId });
+      res.json(business);
+    } catch (err) {
+      console.log(err);
+      res.json({ error: err.message });
     }
   }
 );
